e6e9a64d3a3ea51bf302e3f9c92cdba2ee174c8e
Jamie McCarville <jamiemccarville@gmail.com>
2019-11-26 10:47:45 -0500

    added custom spacing to extend tailwind

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,64 +1,178 @@
 <template>
-  <!-- Column -->
-  <div
-    class="my-1 px-2 py-2 lg:py-0 w-full md:w-1/2 lg:my-4 lg:px-4 xl:px-2 xl:w-1/4"
-  >
-    <!-- Article -->
-    <article class="overflow-hidden rounded-lg shadow-lg hover:shadow-xl zoom">
-      <div class="relative bg-turquoise-500 pb-2/3">
-        <a
-          href="https://codesandbox.io/s/github/jamiemccarville/udemy-vue2-final-project"
-          target="blank"
-        >
-          <img
-            alt=" Placeholder"
-            class="absolute h-full w-full object-cover object-left"
-            src="@/assets/logo.png"
-          />
-        </a>
-      </div>
+  <div class="w-7/8 sm:w-10/12 md:w-11/12 my-12 mx-auto px-4 md:px-12">
+    <div class="flex flex-wrap -mx-1 lg:-mx-4">
+      <!--  -->
 
-      <div class="px-6 py-4">
-        <div class="font-sans font-bold text-xl mb-2">
-          <a
-            class="no-underline hover:underline"
-            href="https://codesandbox.io/s/github/jamiemccarville/udemy-vue2-final-project"
-            target="blank"
-            >Stock Trader</a
-          >
-        </div>
+      <!-- Column -->
+      <div
+        class="my-1 px-2 py-2 lg:py-0 w-full md:w-1/2 lg:my-4 lg:px-4 xl:px-2 xl:w-1/4"
+      >
+        <!-- Article -->
+        <article
+          class="overflow-hidden rounded-lg shadow-lg hover:shadow-xl zoom"
+        >
+          <div class="relative bg-turquoise-500 pb-2/3">
+            <a
+              href="https://codesandbox.io/s/github/jamiemccarville/udemy-vue2-final-project"
+              target="blank"
+            >
+              <img
+                alt=" Placeholder"
+                class="absolute h-full w-full object-cover"
+                src="../assets/hacktoberfest2.png"
+              />
+            </a>
+          </div>
 
-        <p class="text-gray-700 text-base font-opensans">
-          Final project for Vue course building an app using Vue, vue-router,
-          vuex and Firebase
-        </p>
+          <div class="px-6 py-4">
+            <div class="font-sans font-bold text-xl mb-2">
+              <a
+                class="no-underline hover:underline"
+                href="https://codesandbox.io/s/github/jamiemccarville/udemy-vue2-final-project"
+                target="blank"
+                >Stock Trader</a
+              >
+            </div>
+
+            <p class="text-gray-700 text-base font-opensans">
+              Final project for Vue course building an app using Vue,
+              vue-router, vuex and Firebase
+            </p>
+          </div>
+          <div class="px-6 py-4">
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
+              >#Vue</span
+            >
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
+              >#vuex</span
+            >
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700"
+              >#vue-router</span
+            >
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700"
+              >#Firebase</span
+            >
+          </div>
+        </article>
+        <!-- END Article -->
       </div>
-      <div class="px-6 py-4">
-        <span
-          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
-          >#Vue</span
+      <!-- END Column -->
+
+      <!-- Column -->
+      <div
+        class="my-1 px-2 py-2 lg:py-0 w-full md:w-1/2 lg:my-4 lg:px-4 xl:px-2 xl:w-1/4"
+      >
+        <!-- Article -->
+        <article
+          class="overflow-hidden rounded-lg shadow-lg hover:shadow-xl zoom"
         >
-        <span
-          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
-          >#vuex</span
-        >
-        <span
-          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700"
-          >#vue-router</span
-        >
-        <span
-          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700"
-          >#Firebase</span
-        >
+          <div class="relative bg-turquoise-500 pb-2/3">
+            <a href="https://github.com/jamiemccarville" target="blank">
+              <img
+                alt=" Placeholder"
+                class="absolute h-full w-full object-cover"
+                src="../assets/hacktoberfest2.png"
+              />
+            </a>
+          </div>
+
+          <div class="px-6 py-4">
+            <div class="font-sans font-bold text-xl mb-2">
+              <a
+                class="no-underline hover:underline"
+                href="https://github.com/jamiemccarville"
+                target="blank"
+                >Hacktoberfest 2019</a
+              >
+            </div>
+
+            <p class="text-gray-700 text-base font-opensans">
+              Multiple Open Source contributions including Vue Community and
+              Opendrinks.io
+            </p>
+          </div>
+          <div class="px-6 py-4">
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
+              >#open source</span
+            >
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
+              >#git</span
+            >
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700"
+              >#documentation</span
+            >
+          </div>
+        </article>
+        <!-- END Article -->
       </div>
-    </article>
-    <!-- END Article -->
+      <!-- END Column -->
+
+      <!-- Column -->
+      <div
+        class="my-1 px-2 py-2 lg:py-0 w-full md:w-1/2 lg:my-4 lg:px-4 xl:px-2 xl:w-1/4"
+      >
+        <!-- Article -->
+        <article
+          class="overflow-hidden rounded-lg shadow-lg hover:shadow-xl zoom"
+        >
+          <div class="relative bg-turquoise-500 pb-2/3">
+            <a href="https://codepen.io/collection/XBePGG/" target="blank">
+              <img
+                alt=" Placeholder"
+                class="absolute h-full w-full object-cover"
+                src="../assets/hacktoberfest2.png"
+              />
+            </a>
+          </div>
+
+          <div class="px-6 py-4">
+            <div class="font-sans font-bold text-xl mb-2">
+              <a
+                class="no-underline hover:underline"
+                href="https://codepen.io/collection/XBePGG/"
+                target="blank"
+                >freeCodeCamp</a
+              >
+            </div>
+
+            <p class="text-gray-700 text-base font-opensans">
+              Projects that I completed as part of the FCC Responsive Web Design
+              Certification
+            </p>
+          </div>
+          <div class="px-6 py-4">
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
+              >#html</span
+            >
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
+              >#css</span
+            >
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700"
+              >#JS</span
+            >
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700"
+              >#forms</span
+            >
+          </div>
+        </article>
+        <!-- END Article -->
+      </div>
+      <!-- END Column -->
+    </div>
   </div>
-  <!-- END Column -->
 </template>
 
 <script>
 export default {};
 </script>
-
-<style></style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
     <app-card></app-card>
-    <test-card></test-card>
   </div>
 </template>
 
 <script>
 import AppCard from '../components/AppCard';
-import TestCard from '../components/TestCard';
+
 export default {
   components: {
-    AppCard,
-    TestCard
+    AppCard
   }
 };
 </script>
