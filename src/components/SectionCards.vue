d88db5770518918018e92777c6776f7635ae3b5f
Jamie McCarville <jamiemccarville@gmail.com>
2020-01-12 22:06:36 -0500

    updated the list of topics on the functions section card

--- src/components/SectionCards.vue
+++ src/components/SectionCards.vue
@@ -1,28 +1,28 @@
 <template>
-  <div class="w-7/8 sm:w-10/12 md:w-11/12 mx-auto px-4 md:px-12 py-32">
+  <div class="px-4 py-32 mx-auto w-7/8 sm:w-10/12 md:w-11/12 md:px-12">
     <div class="flex flex-wrap -mx-1 lg:-mx-4">
       <!--  -->
 
       <!-- Column -->
-      <div class="my-1 px-2 py-2 lg:py-0 w-full md:w-1/2 lg:my-4 lg:px-4 xl:px-2 xl:w-1/4">
+      <div class="w-full px-2 py-2 my-1 lg:py-0 md:w-1/2 lg:my-4 lg:px-4 xl:px-2 xl:w-1/4">
         <!-- Article -->
         <router-link to="/basics">
           <article
-            class="overflow-hidden rounded-lg shadow-lg hover:shadow-xl zoom border-solid border-gray-700 border"
+            class="overflow-hidden border border-gray-700 border-solid rounded-lg shadow-lg hover:shadow-xl zoom"
           >
             <div class="relative bg-turquoise-500 pb-2/3">
               <img
                 alt=" Placeholder"
-                class="absolute h-full w-full object-cover"
+                class="absolute object-cover w-full h-full"
                 src="../assets/basics-card-topper.png"
               />
             </div>
 
-            <div class="font-normal px-6 py-4 bg-gray-300 border-solid border-gray-700 border-t">
+            <div class="px-6 py-4 font-normal bg-gray-300 border-t border-gray-700 border-solid">
               <div class="w-6/12 mx-auto">
                 <div class="flex justify-center">
                   <ul
-                    class="ml-2 font-semibold text-gray-700 text-base font-opensans text-l list-disc text-left"
+                    class="ml-2 text-base font-semibold text-left text-gray-700 list-disc font-opensans text-l"
                   >
                     <li>What is JS?</li>
                     <li>Data Types</li>
@@ -40,31 +40,34 @@
       <!-- END Column -->
 
       <!-- Column -->
-      <div class="my-1 px-2 py-2 lg:py-0 w-full md:w-1/2 lg:my-4 lg:px-4 xl:px-2 xl:w-1/4">
+      <div class="w-full px-2 py-2 my-1 lg:py-0 md:w-1/2 lg:my-4 lg:px-4 xl:px-2 xl:w-1/4">
         <!-- Article -->
         <router-link to="/functions">
           <article
-            class="overflow-hidden rounded-lg shadow-lg hover:shadow-xl zoom border-solid border-gray-700 border"
+            class="overflow-hidden border border-gray-700 border-solid rounded-lg shadow-lg hover:shadow-xl zoom"
           >
             <div class="relative bg-turquoise-500 pb-2/3">
               <img
                 alt=" Placeholder"
-                class="absolute h-full w-full object-cover"
+                class="absolute object-cover w-full h-full"
                 src="../assets/functions-card-topper.png"
               />
             </div>
 
-            <div class="font-normal px-6 py-4 bg-gray-300 border-solid border-gray-700 border-t">
-              <div class="w-6/12 mx-auto">
+            <div class="px-6 py-4 font-normal bg-gray-300 border-t border-gray-700 border-solid">
+              <div class="w-11/12 mx-auto">
                 <div class="flex justify-center">
                   <ul
-                    class="ml-2 font-semibold text-gray-700 text-base font-opensans text-l list-disc text-left"
+                    class="ml-2 text-base font-semibold text-left text-gray-700 list-disc font-opensans text-l"
                   >
-                    <li>What is JS?</li>
-                    <li>Data Types</li>
-                    <li>Variables</li>
-                    <li>Operators</li>
-                    <li>Comments</li>
+                    <li>What are functions?</li>
+                    <li>Quick Overview</li>
+                    <li>Defining a function</li>
+                    <li>Calling a function</li>
+                    <li>Parameters</li>
+                    <li>Arguments</li>
+                    <li>Return Statement</li>
+                    <li>IIFE</li>
                   </ul>
                 </div>
               </div>
